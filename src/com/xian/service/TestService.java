6e7e2b5bdaf449c7155f133aaded35e4863425bf
xiaoxian120 <363756222@qq.com>
2017-10-13 16:10:05 +0800

    初始导入

---
+++ src/com/xian/service/TestService.java
@@ -0,0 +1,12 @@
+package com.xian.service;
+
+/**
+ * @Author: xian
+ * @Description: 测试类
+ * @Date:create in 2017/10/13 16:03
+ */
+public class TestService {
+    public static void main(String[] args) {
+        System.out.println("哈哈........");
+    }
+}
